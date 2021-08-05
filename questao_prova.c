c4609bd5320573eb8d01dcc84e0504bb76845952
Felipe <narulee@hotmail.com>
2021-08-05 13:50:43 -0300

    remake with matrix stack

--- questao_prova.c
+++ questao_prova.c
@@ -27,42 +27,50 @@ void display(void)
 /*  clear all pixels  */
     glClear(GL_COLOR_BUFFER_BIT);
     glClear(GL_DEPTH_BUFFER_BIT);
-       
-    // glColor3f (0.0, 0.0, 0.0);
-    // glBegin(GL_LINE_STRIP);
-    //     glVertex2f(0.01, 1.0);
-    //     glVertex2f(0.01, 0.01);
-    //     glVertex2f(1.0, 0.01);
-    // glEnd();
+
+    float triangle_w = 1.0, triangle_h = 1.0, black_rectangle_w = 1.0, 
+    black_rectangle_h = 3.0, yellow_rectangle_w = 3.0, yellow_rectangle_h = 1.0;
 
     glPushMatrix();
-    glColor3f (0.0, 0.0, 0.0);
-    glTranslatef(0.5, 0.35, 0.0);       // X = 0.5; Y = 0.3/2 + 0.2 = 0.35
-    glScalef(0.1, 0.3, 1.0);            // X = 0.1; Y = 0.3
-    quadrado();
-    glPopMatrix();
 
-    glPushMatrix();
-    glColor3f (1.0, 1.0, 0.0);
-    glTranslatef(0.5, 0.55, 0.0);   // X = 0.5; Y = 0.2 + 0.3 + 0.1/2 = 0.55
-    glScalef(0.3, 0.1, 1.0);         // X = 0.3; Y = 0.1
-    quadrado();
-    glPopMatrix();
+        // desenha o retangulo preto, iniciando em x = 5 e y = 2
+        glTranslatef(5.0, 2.0 + black_rectangle_h/2.0, 0.0);  
+        glColor3f (0.0, 0.0, 0.0);
+        glPushMatrix();
+            glScalef(black_rectangle_w, black_rectangle_h, 10.0);  
+            quadrado();
+        glPopMatrix();
+   
+        // transalada para imediatamente acima do retangulo preto 
+        glTranslatef(0.0, black_rectangle_h/2.0, 0.0); 
 
-    glPushMatrix();
-    glColor3f (1.0, 0.0, 0.0);  
-    glTranslatef(0.4, 0.6, 0.0);   // X = 0.5 - 0.1; Y = 0.2 + 0.3 + 0.1 = 0.6
-    glScalef(0.1, 0.1, 1.0);       // X = 0.1; Y = 0.1
-    triangulo();
-    glPopMatrix();
+        // desenha o retangulo amarelo
+        glColor3f (1.0, 1.0, 0.0);
+        glTranslatef(0.0, yellow_rectangle_h/2.0, 0.0);
+        glPushMatrix();
+            glScalef(yellow_rectangle_w, yellow_rectangle_h, 10.0);      
+            quadrado();
+        glPopMatrix();
 
-    glPushMatrix();
-    glColor3f (0.0, 0.0, 1.0);
-    glTranslatef(0.6, 0.6, 0.0);    // X = 0.5 + 0.1; Y = 0.2 + 0.3 + 0.1 = 0.6
-    glScalef(0.1, 0.1, 1.0);       // X = 0.1; Y = 0.1
-    triangulo();
-    glPopMatrix();
+        // translada para imediatamente acima do retangulo amarelo
+        glTranslatef(0.0, yellow_rectangle_h/2.0, 0.0); 
+        glScalef(triangle_w, triangle_h, 10.0);     
+        
+        // desenha o triangulo ma extremidade esquerda do retangulo amarelo
+        glPushMatrix();
+            glColor3f (1.0, 0.0, 0.0);  
+            glTranslatef(-yellow_rectangle_w/2.0 + triangle_w/2.0, 0.0, 0.0);   
+            triangulo();
+        glPopMatrix();
 
+        // desenha o triangulo ma extremidade direita do retangulo amarelo
+        glPushMatrix();
+            glColor3f (0.0, 0.0, 1.0);
+            glTranslatef(yellow_rectangle_w/2.0 - triangle_w/2.0, 0.0, 0.0);   
+            triangulo();
+        glPopMatrix();
+
+    glPopMatrix();
 
 /*  don't wait!  
  *  start processing buffered OpenGL routines 
@@ -78,7 +86,7 @@ void init (void)
 /*  initialize viewing values  */
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+    glOrtho(0.0, 10.0, 0.0, 10.0, -1.0, 1.0);
     
 }
 
@@ -95,7 +103,7 @@ int main(int argc, char** argv)
     glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
     glutInitWindowSize (350, 350); 
     glutInitWindowPosition (100, 100);
-    glutCreateWindow ("Prova");
+    glutCreateWindow ("Questão da Prova");
     init();
     glutDisplayFunc(display); 
     glutMainLoop();
